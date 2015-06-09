e3b2c0660d2e2ae6ab4f23bd79610cd7152a3d8b
Jeffrey Licht <jlicht@podconsulting.com>
2015-06-09 14:40:00 -0400

    First commit

---
+++ src/test/java/edu/harvard/lib/librarycloud/test/APIContentTypesTest.java
@@ -0,0 +1,96 @@
+package edu.harvard.lib.librarycloud.test;
+
+import  com.jayway.restassured.RestAssured;
+import com.jayway.restassured.builder.ResponseBuilder;
+// import com.jayway.restassured.itest.java.support.WithJetty;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.parsing.Parser;
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.path.json.exception.JsonPathException;
+import com.jayway.restassured.path.xml.exception.XmlPathException;
+import com.jayway.restassured.response.Headers;
+import com.jayway.restassured.response.Response;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import java.util.List;
+import java.util.Properties;
+
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.config.JsonConfig.jsonConfig;
+import static com.jayway.restassured.config.RestAssuredConfig.newConfig;
+import static com.jayway.restassured.path.json.config.JsonPathConfig.NumberReturnType.BIG_DECIMAL;
+import static com.jayway.restassured.path.json.config.JsonPathConfig.NumberReturnType.FLOAT_AND_DOUBLE;
+import static com.jayway.restassured.path.json.config.JsonPathConfig.jsonPathConfig;
+import static com.jayway.restassured.path.xml.XmlPath.CompatibilityMode.HTML;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class APIContentTypesTest extends APITestBase {
+
+    @Test
+    public void itemTestResponseCodes() throws Exception {
+        get("/items").then().assertThat().statusCode(200);
+        get("/items?q=peanuts").then().assertThat().statusCode(200);        
+    }
+
+    @Test
+    public void collectionsReadTestResponseCodes() throws Exception {
+        get("/collections").then().assertThat().statusCode(200);
+        get("/collections?q=collection").then().assertThat().statusCode(200);        
+    }
+
+    @Test
+    public void itemTestContentTypeDefault() throws Exception {
+        get("/items").then().assertThat().contentType(ContentType.XML);
+    }
+
+    @Test
+    public void itemTestContentTypeExtensions() throws Exception {
+        get("/items.json").then().assertThat().contentType(ContentType.JSON);
+        get("/items.xml").then().assertThat().contentType(ContentType.XML);
+    }
+
+	@Test
+    public void itemTestContentTypeQueryWithExtensions() throws Exception {
+		get("/items?q=peanuts").then().assertThat().contentType(ContentType.XML);
+        get("/items.json?q=peanuts").then().assertThat().contentType(ContentType.JSON);
+        get("/items.xml?q=peanuts").then().assertThat().contentType(ContentType.XML);
+    }
+
+	@Test
+    public void itemTestContentTypeWithContentTypeRequested() throws Exception {
+        given().header("Accept", "application/json").when().get("/items").then().assertThat().contentType(ContentType.JSON);
+        given().header("Accept", "application/xml").when().get("/items").then().assertThat().contentType(ContentType.XML);
+    }
+
+    @Test
+    public void collectionsTestContentTypesDefault() throws Exception {
+        /* Our default for collections is JSON.... */
+        get("/collections").then().assertThat().contentType(ContentType.JSON);
+    }
+
+    @Test
+    public void collectionsTestContentTypesWithExtensions() throws Exception {
+        get("/collections.json").then().assertThat().contentType(ContentType.JSON);
+        get("/collections.xml").then().assertThat().contentType(ContentType.XML);
+    }
+
+    @Test
+    public void collectionsTestContentTypeWithContentTypeRequested() throws Exception {
+        given().header("Accept", "application/xml").when().get("/items").then().assertThat().contentType(ContentType.XML);
+        given().header("Accept", "application/json").when().get("/items").then().assertThat().contentType(ContentType.JSON);
+    }
+
+
+
+}
---
+++ src/test/java/edu/harvard/lib/librarycloud/test/APIPaginationTest.java
@@ -0,0 +1,60 @@
+package edu.harvard.lib.librarycloud.test;
+
+import  com.jayway.restassured.RestAssured;
+import com.jayway.restassured.builder.ResponseBuilder;
+// import com.jayway.restassured.itest.java.support.WithJetty;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.parsing.Parser;
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.path.json.exception.JsonPathException;
+import com.jayway.restassured.path.xml.exception.XmlPathException;
+import com.jayway.restassured.response.Headers;
+import com.jayway.restassured.response.Response;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import java.util.List;
+import java.util.Properties;
+
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.config.JsonConfig.jsonConfig;
+import static com.jayway.restassured.config.RestAssuredConfig.newConfig;
+import static com.jayway.restassured.path.json.config.JsonPathConfig.NumberReturnType.BIG_DECIMAL;
+import static com.jayway.restassured.path.json.config.JsonPathConfig.NumberReturnType.FLOAT_AND_DOUBLE;
+import static com.jayway.restassured.path.json.config.JsonPathConfig.jsonPathConfig;
+import static com.jayway.restassured.path.xml.XmlPath.CompatibilityMode.HTML;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class APIPaginationTest extends APITestBase {
+
+    @Test
+    public void itemTestPaginationDefaultsWithAllEncodi() throws Exception {
+        get("/items.json").then().assertThat().body("pagination.limit",equalTo(10));
+        get("/items").then().assertThat().body("results.pagination.limit.text()",equalTo("10"));
+        get("/items.json").then().assertThat().body("pagination.start",equalTo(0));
+        get("/items").then().assertThat().body("results.pagination.start.text()",equalTo("0"));
+    }
+
+	@Test
+    public void itemTestPaginationSpecifyStartLimit() throws Exception {
+        get("/items.json?start=5").then().assertThat().body("pagination.start",equalTo(5));
+        get("/items.json?limit=5").then().assertThat().body("pagination.limit",equalTo(5));
+        get("/items.json?limit=2&start=3").then().assertThat().body("pagination.limit",equalTo(2)).body("pagination.start",equalTo(3));
+    }
+
+    @Test
+    public void itemTestPaginationMaximumPageSize() throws Exception {
+        get("/items.json?limit=300").then().assertThat().body("pagination.limit",equalTo(250));
+    }
+
+}
